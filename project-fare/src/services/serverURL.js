5ee46f5a6298ee4427d01bc8d78ca355a9a7eccf
I-DUDE-I <thejusmathew8888@gmail.com>
2024-12-04 16:28:53 +0530

    updated server url

--- project-fare/src/services/serverURL.js
+++ project-fare/src/services/serverURL.js
@@ -1,2 +1,2 @@
 // export const serverURL = 'http://localhost:3000'
-export const serverURL = 'https://project-fair-backend-160d.onrender.com'
+export const serverURL = 'https://project-fare-fullstack.onrender.com'
